ae3f6a7cf5613b26de152da760c1a22b25c4eaba
pefish <darrondarron@qq.com>
2024-03-19 09:19:10 +0800

    v1.1.1

--- vm_test.go
+++ vm_test.go
@@ -1,47 +1,17 @@
 package vm
 
 import (
-	"github.com/pefish/go-test"
 	"strings"
 	"testing"
+
+	go_test_ "github.com/pefish/go-test"
 )
 
 func TestNewVm(t *testing.T) {
 	vm, err := NewVm(`
 console.log("123")
 `)
-	test.Equal(t, nil, err)
-	_, err = vm.Run(nil)
-	test.Equal(t, true, err != nil && strings.Contains(err.Error(), "js function <main> not be found"))
-
-	vm1, err := NewVm(`
-function main(args) {
-console.log(args)
-return args
-}
-`)
-	test.Equal(t, nil, err)
-	result, err := vm1.Run([]interface{}{"111", "222", 123})
-	test.Equal(t, nil, err)
-	realResult, ok := result.([]interface{})
-	test.Equal(t, true, ok)
-	test.Equal(t, "111", realResult[0].(string))
-	test.Equal(t, "222", realResult[1].(string))
-	test.Equal(t, 123, realResult[2].(int))
-
-	vm2, err := NewVm(`
-function main(args) {
-	return regex.match(args[0], args[1])
-}
-`)
-	test.Equal(t, nil, err)
-	_, err = vm2.Run([]interface{}{"*", "222"})
-	test.Equal(t, true, err != nil && strings.Contains(err.Error(), "function main run failed - error parsing regexp: missing argument to repetition operator"))
-
-	result21, err := vm2.Run([]interface{}{".*", "222"})
-	test.Equal(t, nil, err)
-	realResult21, ok := result21.(bool)
-	test.Equal(t, true, ok)
-	test.Equal(t, true, realResult21)
-
+	go_test_.Equal(t, nil, err)
+	_, err = vm.Run()
+	go_test_.Equal(t, true, err != nil && strings.Contains(err.Error(), "js function <main> not be found"))
 }
