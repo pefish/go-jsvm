b986b44cc7311b145abc514ecdd98a8d96579a6b
pefish <darrondarron@qq.com>
2024-06-20 16:43:12 +0800

    add

--- vm_test.go
+++ vm_test.go
@@ -8,10 +8,9 @@ import (
 )
 
 func TestNewVm(t *testing.T) {
-	vm, err := NewVm(`
+	vm := NewVm(`
 console.log("123")
 `)
-	go_test_.Equal(t, nil, err)
-	_, err = vm.Run()
+	_, err := vm.Run()
 	go_test_.Equal(t, true, err != nil && strings.Contains(err.Error(), "js function <main> not be found"))
 }
